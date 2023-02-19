dfe1d98448935703013f13a82c043c52c97c0bd4
PanMikolaj <100166221+PanMikolaj@users.noreply.github.com>
2023-02-20 00:38:53 +0100

    oferusy susy amongusy
    :)

--- src/app/ofertus/ofertus.component.ts
+++ src/app/ofertus/ofertus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-ofertus',
@@ -8,8 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class OfertusComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit(): void {
+  oferty: any = []
+  testUs = {
+    tytul: "Sporzntako",
+    miasto: "Afrika",
+    wynagrodzenie: "10",
+    opis: "Potrzebuje kogoś do sprzątania polskiego cmenatrza we wiosce śnieżynek",
+    dokiedy: "1.09.1939",
+    pfp: "assets/pfptest.png",
+    id_oferty: 1
   }
+  testUs2 = {
+    tytul: "AMONGUS",
+    miasto: "STACJA KOSMICZNA",
+    wynagrodzenie: "2137",
+    opis: "IMPOSTOR IS AMONGUS!!!!!!!!!!!!",
+    dokiedy: "NOW!!!!!!!",
+    pfp: "assets/amongus.png",
+    id_oferty: 2
+  }
+  ngOnInit(): void {
+    this.oferty.push(this.testUs)
+    this.oferty.push(this.testUs2)
+    this.oferty.push(this.testUs)
+    this.oferty.push(this.testUs2)
+    this.oferty.push(this.testUs)
+    this.oferty.push(this.testUs2)
+    this.oferty.push(this.testUs)
+    this.oferty.push(this.testUs2)
+    
 
+  }
 }
