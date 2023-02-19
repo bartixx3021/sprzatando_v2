76738c153e015285f0c0943d3372300f4da0d829
Bartix3021 <bartekfopke@gmail.com>
2023-02-19 18:55:47 +0100

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomusComponent } from './homus/homus.component';
+import { LoginusComponent } from './loginus/loginus.component';
+import { OfertusComponent } from './ofertus/ofertus.component';
+import { RegistrusComponent } from './registrus/registrus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "homus", component: HomusComponent},
+  {path: "loginus", component: LoginusComponent},
+  {path: "ofertus", component: OfertusComponent},
+  {path: "registrus", component: RegistrusComponent},
+  {path: "..", component: HomusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
