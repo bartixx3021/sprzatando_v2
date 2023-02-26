8997a11d7a3d6fd362552613ec17d1f649c4c04d
Bartix3021 <bartekfopke@gmail.com>
2023-02-26 16:59:09 +0100

    homus

--- src/app/homus/homus.component.ts
+++ src/app/homus/homus.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HomusComponent implements OnInit {
 
   constructor() { }
-
+  oferty :any[] = [];
   ngOnInit(): void {
+    let url = "http://130.162.234.221:8080?action=offer&subact=select&security=ezzz";
+    fetch(url).then(stream => stream.json()).then(jsonData => {
+      let ans = jsonData;
+      console.log(ans);
+      let x = ans.result.reverse();
+      for (let i = 0; i < 7; i++) {
+        this.oferty.push(x[i]);
+      }
+    })
   }
+
+
 
 }
