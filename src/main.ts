fc9ac77b1999fa00fa41da0d477e25e6c3724d8e
Bartix3021 <bartekfopke@gmail.com>
2023-05-25 18:10:57 +0200

    ddd

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  ServiceWorkerModule.register('ngsw-worker.js');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
