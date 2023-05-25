49e500f3394c698395e983ab4588d8be61775fb1
Bartix3021 <bartekfopke@gmail.com>
2023-05-25 16:05:25 +0200

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MenusComponent } from './menus/menus.component';
 import { OfertuspodgladusComponent } from './ofertuspodgladus/ofertuspodgladus.component';
 import { RankingusComponent } from './rankingus/rankingus.component';
 import { ProfilusComponent } from './profilus/profilus.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,13 @@ import { ProfilusComponent } from './profilus/profilus.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
